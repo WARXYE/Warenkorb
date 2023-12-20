80dd114d2508a46faba535395e077f5937959976
NoLifeDetected <148354993+NoLifeDetected@users.noreply.github.com>
2023-12-20 11:16:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Store</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body class="container">
+
+    <h1 class="mt-5">Welcome to the Online Store</h1>
+
+    <!-- Product Listing -->
+    <ul class="list-group mt-3">
+        <li class="list-group-item">Product 1 - $20 <a href="cart.php?action=add&id=1" class="btn btn-primary btn-sm float-right">Add to Cart</a></li>
+        <li class="list-group-item">Product 2 - $30 <a href="cart.php?action=add&id=2" class="btn btn-primary btn-sm float-right">Add to Cart</a></li>
+        <!-- Add more products as needed -->
+    </ul>
+
+</body>
+</html>
